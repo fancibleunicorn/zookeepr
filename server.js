5aadd7136eb29c9a79d48a7f7039fe9ebf239423
Adam Crandall <adamcrandall91@gmail.com>
2020-12-15 19:43:03 -0500

    finished 11.2

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // get access to Express.js
 const express = require('express');
 
+// need fs to write new animals to animals.json
+const fs = require('fs');
+
+// node built into Node.js for workith with file and directory paths
+const path = require('path');
+
 // get access to animals.js
 const { animals } = require('./data/animals.json');
 
@@ -9,6 +15,12 @@ const PORT = process.env.PORT || 3001;
 
 // to initiate the server
 const app = express();
+
+//Middleware functions:
+// parse incoming string or array data (extended data true tells it to parse all data, including sub-arrays)
+app.use(express.urlencoded({ extended: true}));
+//parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     // for handling searching info stored in an array
@@ -55,6 +67,40 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+
+    //push new animal to array
+    animalsArray.push(animal);
+    //write new animal to animals.json
+    fs.writeFileSync(
+        // creates pathway to where the animals.json file is stored
+        path.join(__dirname, './data/animals.json'),
+        //converts javascript array to JSON, null means we don't want to edit existing data, and 2 mean we want to create white space between values to make readable
+        JSON.stringify({ animals: animalsArray}, null, 2)
+    );
+    
+    //return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+
+}
+
 // route to animals.js, get() requires 2 arguments: a string to describe the route to fetch from, and a callback function that will execute every time the route is accessed with a GET request
 app.get('/api/animals', (req, res) => {
     // use send() from res parameter to send string
@@ -87,7 +133,25 @@ app.get('/api/animals/:id', (req, res) => {
     }
 })
 
-// method to make server listen
+// for adding user input to the server (i.e. new animals)
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+
+    //set's new animal's id to next highest number
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        //add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+
+        res.json(animal);
+    }
+});
+
+// method to make server listen (traditionally at the very end of your script)
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
